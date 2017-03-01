e7f69d5f44145ea7f91561e014785d5f9e2f04c2
root <root@localhost.localdomain>
2017-03-01 01:12:40 +0000

    Executed Perl file with basic stdout

--- server/routes/api.js
+++ server/routes/api.js
@@ -10,17 +10,20 @@ router.get('/inclexcl', (req, res) => {
   var exclu = req.param('excl');
   var pa = path.resolve(path.join(__dirname, "/perl/test.py"));
 	var py = exec("python " + pa, function (err,stdout,stderr){
-    console.log(err);
-    console.log(stdout);
-    console.log(stderr);
 	});
 	py.stdin.write(JSON.stringify(inclu +";"+ exclu));
 	py.stdin.end();
   py.stdout.on('data', function(data){
-		dataPrint += data.toString();
-		res.send(dataPrint);
+    dataPrint = data.replace(/\r?\n|\r/g, "");
+		if(dataPrint == "done") {
+      var pt = path.resolve(path.join(__dirname, "/perl/perlPrint.pl"));
+      var prun = exec("perl " + pt, function(err,stdout,stderr){
+      });
+      prun.stdout.on('data', function(data){
+        res.send(data.toString());
+      });
+    }
 	});
-
 });
 
 module.exports = router;
